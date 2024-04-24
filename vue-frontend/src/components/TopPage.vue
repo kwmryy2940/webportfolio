365ec5b38325c8a36cb107b584b7043eb6a063d8
kwmryy2940 <mailbox.kawamura@gmail.com>
2024-04-24 17:16:53 +0900

    ヘッダーのツールバーにハンバーガーメニューを作成し、その中にWork、Skillのメニューを追加

--- vue-frontend/src/components/TopPage.vue
+++ vue-frontend/src/components/TopPage.vue
@@ -1,44 +1,49 @@
 <script setup>
 import { ref } from "vue";
 
-const drawer = ref(false);  // ナビゲーションドロワーの表示を制御する変数
+const drawer = ref(false); // ナビゲーションドロワーの表示を制御する変数
+
+const navigationItems = ref([
+  { text: "Work", icon: "mdi-briefcase" },
+  { text: "Skill", icon: "mdi-pen" },
+]);
 </script>
 
 <template>
   <v-app>
-    <v-app-bar color="primary" :elevation="2">
+    <v-navigation-drawer v-model="drawer" temporary>
+      <v-list>
+        <v-list-subheader>MENU</v-list-subheader>
+        <v-list-item
+          v-for="(item, i) in navigationItems"
+          :key="i"
+          :value="item"
+          color="primary"
+        >
+          <template v-slot:prepend>
+            <v-icon :icon="item.icon"></v-icon>
+          </template>
+
+          <v-list-item-title v-text="item.text"></v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      :order="order"
+      color="primary"
+      :elevation="2"
+      scroll-behavior="inverted hide"
+    >
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
       <v-app-bar-title>My portofolio</v-app-bar-title>
     </v-app-bar>
 
-    <v-navigation-drawer v-model="drawer" temporary>
-      <v-list nav dense>
-        <v-list-item-group
-          v-model="group"
-          active-class="deep-purple--text text--accent-4"
-        >
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-briefcase</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Account</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
-
     <v-main>
       <v-container fluid class="d-flex align-center justify-center">
         Main Contents
       </v-container>
+      <v-container style="height: 1000px"></v-container>
     </v-main>
   </v-app>
 </template>
