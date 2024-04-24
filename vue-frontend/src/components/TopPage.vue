b25c7a77292a03e90718b2b32d9e9fd69fc23be8
kwmryy2940 <mailbox.kawamura@gmail.com>
2024-04-24 16:32:12 +0900

    ハンバーガーメニュー、ナビゲーションドロワーを追加

--- vue-frontend/src/components/TopPage.vue
+++ vue-frontend/src/components/TopPage.vue
@@ -1,8 +1,39 @@
+<script setup>
+import { ref } from "vue";
+
+const drawer = ref(false);  // ナビゲーションドロワーの表示を制御する変数
+</script>
+
 <template>
   <v-app>
-    <v-app-bar color="primary">
+    <v-app-bar color="primary" :elevation="2">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
       <v-app-bar-title>My portofolio</v-app-bar-title>
     </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" temporary>
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-briefcase</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Account</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-main>
       <v-container fluid class="d-flex align-center justify-center">
