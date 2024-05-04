b3aaaf37b1320a7725c928946c9922a88f61601f
kwmryy2940 <mailbox.kawamura@gmail.com>
2024-05-04 21:25:21 +0900

    成果物ページのデザイン修正、ファビコン設定

--- vue-frontend/src/components/Work.vue
+++ vue-frontend/src/components/Work.vue
@@ -8,11 +8,11 @@
         <v-col cols="12" sm="8" md="6" lg="4" xl="3">
           <v-card class="fill-height" elevation="2" href="#top">
             <v-img
-              :width="300"
+              style="width: 100%"
               :height="150"
               :src="workImg1"
-              aspect-ratio="4/3"
               cover
+              contain
             ></v-img>
             <v-card-title>portofolio</v-card-title>
             <v-card-text
@@ -30,11 +30,11 @@
             rel="noopener noreferrer"
           >
             <v-img
-              :width="300"
+              style="width: 100%"
               :height="150"
               :src="workImg2"
-              aspect-ratio="4/3"
               cover
+              contain
             ></v-img>
             <v-card-title>Timer App</v-card-title>
             <v-card-text> シンプルなタイマーアプリです。</v-card-text>
@@ -49,11 +49,11 @@
             rel="noopener noreferrer"
           >
             <v-img
-              :width="300"
+              style="width: 100%"
               :height="150"
               :src="workImg3"
-              aspect-ratio="4/3"
               cover
+              contain
             ></v-img>
             <v-card-title>Bingo</v-card-title>
             <v-card-text> 1～75の範囲でビンゴができるアプリです。</v-card-text>
