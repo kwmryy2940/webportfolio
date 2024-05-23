ddabc9d744ae9c7365b99e3e1d6fa1f4588ca56b
kwmryy2940 <mailbox.kawamura@gmail.com>
2024-05-23 10:42:06 +0900

    add work

--- vue-frontend/src/components/Work.vue
+++ vue-frontend/src/components/Work.vue
@@ -79,6 +79,29 @@
             </v-card-text>
           </v-card>
         </v-col>
+        <v-col cols="12" sm="8" md="6" lg="4" xl="3">
+          <v-card
+            class="fill-height"
+            elevation="2"
+            href="https://kwmryy2940.github.io/animedb/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <v-img
+              style="width: 150%"
+              :height="150"
+              :src="workImg5"
+              contain
+            ></v-img>
+            <v-card-title>アニメ検索</v-card-title>
+            <v-card-text>
+              <p>放送年・放送シーズン・タイトルをぼり込んでアニメを検索できるアプリです。</p>
+              <p>
+                作品情報はAnnict API、キャストやスタッフ情報はしょぼいカレンダーAPIを使用して取得しています。
+              </p>
+            </v-card-text>
+          </v-card>
+        </v-col>
       </v-row>
     </v-container>
   </div>
@@ -89,9 +112,11 @@ import imageSource1 from "../assets/img/work/portofolio.svg";
 import imageSource2 from "../assets/img/work/timer.svg";
 import imageSource3 from "../assets/img/work/bingo.svg";
 import imageSource4 from "../assets/img/work/dreamdiary.svg"
+import imageSource5 from "../assets/img/work/animedb.svg"
 
 const workImg1 = imageSource1;
 const workImg2 = imageSource2;
 const workImg3 = imageSource3;
 const workImg4 = imageSource4;
+const workImg5 = imageSource5;
 </script>
